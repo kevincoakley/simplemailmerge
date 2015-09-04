46361c7c9a6bca64bee1102bb071dbbf7922e0fc
Kevin Coakley <kcoakley@sdsc.edu>
2015-09-04 14:40:22 -0700

    Test for the correct properties and values

--- simplemailmerge/template.py
+++ simplemailmerge/template.py
@@ -6,11 +6,16 @@ import json
 class Template:
 
     def __init__(self, json_file):
-        self.json = Template.read(json_file)
+        self.from_address = None
+        self.subject = None
+        self.body = None
 
-    @staticmethod
-    def read(json_file):
+        Template.read(self, json_file)
+
+    def read(self, json_file):
         with open(json_file, 'r') as f:
             template_json = json.load(f)
 
-        return None
+        self.from_address = template_json["from_address"]
+        self.subject = template_json["subject"]
+        self.body = template_json["body"]
--- tests/test_template.py
+++ tests/test_template.py
@@ -4,10 +4,18 @@ from simplemailmerge.template import Template
 
 class TemplateTestCase(unittest.TestCase):
     def setUp(self):
+        self.bad_property_json_file = "tests/json/bad_property_json_file.json"
         self.blank_json_file = "tests/json/blank_json_file.json"
         self.invalid_json_file = "tests/json/invalid_json_file.json"
         self.missing_json_file = "tests/json/missing_json_file.json"
+        self.missing_property_json_file = "tests/json/missing_property_json_file.json"
         self.valid_json_file = "tests/json/valid_json_file.json"
+
+        self.json_template_properties = ["body", "from_address", "subject"]
+        self.json_values = {"from_address": "kcoakley@sdsc.edu",
+                            "subject": "I have a link for you!",
+                            "body": "Hi %%FIRST_NAME%%\n\nI have a link to share with you: "
+                                    "%%LINK%%\n\nKevin"}
 
     # User specifies json file that exists
     def test_json_file_exists(self):
@@ -24,6 +32,21 @@ class TemplateTestCase(unittest.TestCase):
         self.assertRaises(ValueError, Template, self.invalid_json_file)
         self.assertIsInstance(Template(self.valid_json_file), Template)
 
-    # The json file has variables: from, subject, body
+    # The json file has properties: from, subject, body
+    def test_json_file_has_correct_properties(self):
+        self.assertRaises(KeyError, Template, self.bad_property_json_file)
+        self.assertRaises(ValueError, Template, self.missing_property_json_file)
 
-    # The values from the csv file are successfully merged with the json template
+        template = Template(self.valid_json_file)
+
+        self.assertListEqual(template.__dict__.keys(), self.json_template_properties)
+
+        for json_template_property in self.json_template_properties:
+            self.assertIsNotNone(template.__dict__[json_template_property])
+
+    # Verify the Template object has the correct values for the properties
+    def test_for_correct_values(self):
+        template = Template(self.valid_json_file)
+
+        for key, value in self.json_values.iteritems():
+            self.assertEqual(template.__dict__[key], value)
